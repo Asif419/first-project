019656877957684b5798707a4a4324d78e23b786
asif419 <asshah419@gmail.com>
2024-11-26 06:48:50 +0200

    creating user(interface, model, validation) done

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'faculty', 'student'],
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model<TUser>('User', userSchema);
