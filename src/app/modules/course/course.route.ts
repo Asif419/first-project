f83102e3bbece6223346c1075a041ce40a38a89f
asif419 <asshah419@gmail.com>
2025-01-09 01:38:56 +0200

    course route update with auth

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -2,24 +2,37 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { CourseValidations } from './course.validation';
 import { CourseControllers } from './course.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 const router = express.Router();
 
 router.post(
   '/create-course',
+  auth(USER_ROLE.admin),
   validateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 );
 
-router.get('/', CourseControllers.getAllCourses);
-router.get('/:id', CourseControllers.getSingleCourseByID);
+router.get(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+  CourseControllers.getAllCourses,
+);
+router.get(
+  '/:id',
+  auth(USER_ROLE.admin),
+  CourseControllers.getSingleCourseByID,
+);
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin),
   validateRequest(CourseValidations.updateCourseValidationSchema),
   CourseControllers.updateCourse,
 );
-router.delete('/:id', CourseControllers.deleteCourse);
+router.delete('/:id', auth(USER_ROLE.admin), CourseControllers.deleteCourse);
 router.put(
   '/:id/assign-faculties',
+  auth(USER_ROLE.admin),
   validateRequest(CourseValidations.FacultiesWithCourseVAlidationSchema),
   CourseControllers.assignFacultiesWithCourse,
 ); //use put: if exist update it otherwise create new
